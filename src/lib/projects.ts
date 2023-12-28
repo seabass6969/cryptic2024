6b61186c20c0ccddf845ee1467c5e33335ecc618
BuildTools <cephas8080@outlook.com>
2023-12-28 13:08:58 +0000

    tracking

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -3,23 +3,27 @@ export const projectlist = [
         link: "https://egg.cephas.monster",
         src: "/assets/egg.webp",
         description: "This is easter egg generator. create by svelte",
+        source_code: "https://github.com/seabass6969/easter_egg_generator/"
     },
     {
         link: "https://Tamagotchi.cephas.monster",
         src: "/assets/tamagotchi.webp",
         description: "This is a Tamagotchi clone on the web",
+        source_code: "https://github.com/seabass6969/Tamagotchi/"
     },
     {
         link: "https://r.cephas.monster",
         src: "/assets/rick_gen.webp",
         description:
             "This is a rick roll generator on links and even QR code. 🤜🤛",
+        source_code: "https://github.com/seabass6969/rick-roll"
     },
     {
         link: "https://gamedle.cephas.monster",
         src: "/assets/gamedle.webp",
         description:
             "Gamedle is a game title guessing game. Inspire by wordle",
+        source_code: ""
     },
     {
         link: "https://sky.cephas.monster",
